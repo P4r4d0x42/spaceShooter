b4b7bc1b8d1c416ffbd04b295e09e19db85fad05
Brandon Fallout <p4r4d0x42@gmail.com>
2012-06-04 01:46:02 -0700

    Added enemy waves
    There are 5 waves that attack the player. Most of the changes were in
    gameManager.js

--- Assets/_scripts/gameManager.js
+++ Assets/_scripts/gameManager.js
@@ -7,16 +7,32 @@
 
 var enemySpwn1  : Transform;
 var enemyTarg1  : Transform;
-var enemyPrefab : GameObject;
-var targetDir   : Vector3;
-private var enemySpeed = 0.1;
+var enemySpwn2  : Transform;
+var enemyTarg2  : Transform;
+var enemyPrefab1 : GameObject;
+var enemyPrefab2 : GameObject;
+var enemyPrefab3 : GameObject;
+var enemySpeed = 0.1;
+var enemySpeed2 = 0.2;
+
+private var targetSpwnDir1   : Vector3;
+private var targetSpwnDir2   : Vector3;
 
 
 function Start () 
 {
-	targetDir = enemyTarg1.position - enemySpwn1.transform.position;
-	SendEnemy();
+	targetSpwnDir1 = enemyTarg1.position - enemySpwn1.transform.position;
+	targetSpwnDir2 = enemyTarg2.position - enemySpwn2.transform.position;
 	
+	SendWave1();
+	yield WaitForSeconds(5);
+	SendWave2();
+	yield WaitForSeconds(5);
+	SendWave3();
+	yield WaitForSeconds(5);
+	SendWave4();
+	SendWave5();
+
 }
 
 function Update () 
@@ -24,8 +40,55 @@ function Update ()
 
 }
 
+function SendWave1()
+{
+	for (var i = 0; i <= 3; i++)
+	{
+		var instantiatedProjectitle : GameObject = Instantiate (enemyPrefab1,enemySpwn1.transform.position, this.transform.rotation);
+		instantiatedProjectitle.rigidbody.velocity = transform.TransformDirection(targetSpwnDir1*enemySpeed2);
+		yield WaitForSeconds(1.0);
+	}
+}
+
+function SendWave2()
+{
+	for (var i = 0; i <= 5; i++)
+	{
+		var instantiatedProjectitle : GameObject = Instantiate (enemyPrefab2,enemySpwn2.transform.position, this.transform.rotation);
+		instantiatedProjectitle.rigidbody.velocity = transform.TransformDirection(targetSpwnDir2*enemySpeed);
+		yield WaitForSeconds(1.0);
+	}
+}
+
+function SendWave3()
+{
+		var instantiatedProjectitle : GameObject = Instantiate (enemyPrefab3,enemySpwn1.transform.position, this.transform.rotation);
+		instantiatedProjectitle.rigidbody.velocity = transform.TransformDirection(targetSpwnDir1*enemySpeed2);
+		yield WaitForSeconds(1.0);
+}
+
+function SendWave4()
+{
+	for (var i = 0; i <= 7; i++)
+	{
+		var instantiatedProjectitle : GameObject = Instantiate (enemyPrefab2,enemySpwn2.transform.position, this.transform.rotation);
+		instantiatedProjectitle.rigidbody.velocity = transform.TransformDirection(targetSpwnDir2*enemySpeed);
+		yield WaitForSeconds(1.0);
+	}
+}
+
+function SendWave5()
+{
+		var instantiatedProjectitle : GameObject = Instantiate (enemyPrefab3,enemySpwn1.transform.position, this.transform.rotation);
+		instantiatedProjectitle.rigidbody.velocity = transform.TransformDirection(targetSpwnDir1*enemySpeed2);
+		yield WaitForSeconds(1.0);
+}
 
 
+
+
+
+/** Not needed anymore
 function SendEnemy()
 {
 	var instantiatedProjectile : GameObject = Instantiate(enemyPrefab, enemySpwn1.transform.position, this.transform.rotation);
@@ -33,3 +96,4 @@ function SendEnemy()
 	instantiatedProjectile.rigidbody.velocity = transform.TransformDirection(targetDir*enemySpeed);
 	
 }
+**/
